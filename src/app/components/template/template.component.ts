c1e42e9058c7b06a82d7970b0c8319b6104f845f
Daniela Garcia <danielagarcia63@gmail.com>
2019-05-12 20:23:41 -0400

    validación de formulario con template[

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,15 +4,35 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent {
 
   usuario:Object = {
-    nombre: 'Daniela',
-    apellido: 'Garcia',
-    correo: ''
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: '',
+    sexo: 'Hombre',
+    acepta: false
+  };
+
+  paises = [{
+    codigo: 'CRI',
+    nombre: 'Costa Rica'
+  },
+  {
+    codigo: 'ESP',
+    nombre: 'Espana'
   }
+];
+
+  sexos:string[] = ['Hombre', 'Mujer'];
+
   constructor() { }
   
   guardar(forma:NgForm){
