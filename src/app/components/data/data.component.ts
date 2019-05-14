d79446c09b4ab03d7c46ec62b9b82daa102cc29a
daniela <danielagarcia63@gmail.com>
2019-05-14 14:00:11 -0400

    Validaciones de formulario con data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,7 +11,10 @@ export class DataComponent{
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required),
+      'nombre': new FormControl('', [
+                                      Validators.required,
+                                      Validators.minLength(3)
+                                    ]),
       'apellido': new FormControl('', Validators.required),
       'correo': new FormControl('', [
                                       Validators.required, 
