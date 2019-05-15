bd466e252724cbe0eaffb74ddc2bbe1d6d3722ba
daniela <danielagarcia63@gmail.com>
2019-05-15 13:55:15 -0400

    reset y carga de dato del formulario

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -21,21 +21,30 @@ export class DataComponent{
     console.log(this.usuario);
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl(this.usuario.nombrecompleto.nombre, [
+        'nombre': new FormControl('', [
           Validators.required,
           Validators.minLength(3)
         ]),
-        'apellido': new FormControl(this.usuario.nombrecompleto.apellido, Validators.required)
+        'apellido': new FormControl('', Validators.required)
       }),     
-      'correo': new FormControl(this.usuario.correo, [
+      'correo': new FormControl('', [
                                       Validators.required, 
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
                                     ])
     })
+
+    this.forma.setValue( this.usuario );
    }
 
    guardarCambios(){
      console.log(this.forma.value);
      console.log(this.forma);
+     this.forma.reset({
+        nombrecompleto:{
+          nombre:"",
+          apellido:""
+        },
+        correo:""
+     });
    }
 }
